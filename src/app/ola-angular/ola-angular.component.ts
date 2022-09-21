af6680c2db4a3964229f072f2a5820cad3a5d5ab
Rodrigo Bossini <professorbossini@gmail.com>
2022-09-21 16:31:39 -0300

    feat(apostila04): fim da listagem 2.2.7
    -componente OlaAngular criado manualmente
    -template definido no arquivo .ts

---
+++ src/app/ola-angular/ola-angular.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'ola-angular',
+  template: `
+    <div style="width: 1280px; margin: auto; text-align:center">
+      <h2>Olá, Angular</h2>
+      <hr>
+    </div>
+  `
+})
+export class OlaAngularComponent{
+  framework = "React"
+}
